2f6bcade47e9656331ed2e0a140b187daa86f6c2
Jaloliddin <isroilovjaloliddin7@gmail.com>
2024-07-19 16:11:38 +0300

    first commit

--- trans bot/tract.py
+++ trans bot/tract.py
@@ -1,10 +1,10 @@
 from transliterate import to_cyrillic, to_latin, transliterate
 import regex
 
-check = bool(regex.search(r'\p{IsCryilic}', 'salom'))
+check = bool(regex.search(r'\p{IsCyrilic}', 'salom'))
 
 def cril_or_latin(text):
-  check = bool(regex.search(r'\p{IsCryilic}', 'salom'))
+  check = bool(regex.search(r'\p{IsCyrilic}', 'salom'))
   if check == True:
        result = to_latin(text)
   else:
